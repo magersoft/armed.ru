c35242f30cea3892eb0713319e7e56c284f0447d
yuvmironov <146@armed.ru>
2018-01-16 09:57:01 +0300

    Начало верстки модального окна для выбора города

--- pre-relise/site.js
+++ pre-relise/site.js
@@ -10255,6 +10255,31 @@ return jQuery;
 
 
 
+$(function(){
+    $('.modalLink').click(function(){
+        var element_id = $(this).attr('data-id');
+        var element_close = $(this).attr('data-id');
+        //получение id для закрытия модального окна
+        element_close = '#close_' + element_close;
+        //получение id для открытия модального окна
+        element_id = '#modal_' + element_id;
+        console.log(element_close);
+        console.log(element_id);
+
+
+        $(function(){
+            $(element_id).fadeIn(400);
+            $('body').css('overflow', 'hidden');
+            $('main').addClass('blurBody');
+        });
+        $(element_close).click(function(){
+            $(element_id).fadeOut(400);
+            $('body').css('overflow', 'auto');
+            $('main').removeClass('blurBody');
+        });
+        return false
+    });
+});
 $(window).on('load', function () {
    $('.preload').delay(500).fadeOut(1000);
 });
