72740ed6459793c8c42258e9e1d50e0b19592cf4
ARMED\mager_v <147@armed.ru>
2018-01-22 16:13:09 +0300

    header search2

--- app/include/Global/search/search.js
+++ app/include/Global/search/search.js
@@ -3,18 +3,22 @@ $(".search__content").mCustomScrollbar({
     scrollbarPosition: "outside"
 });
 
-$('#city').focusin(function(){
+$('#city, #search').focusin(function(){
     var search = $(this).val();
     if (search.length >= 2) {
-        $('.popupCitySelection__search').removeClass('search_hide').addClass('search_active');
+        $('.search').removeClass('search_hide').addClass('search_active');
+        $('#search').animate({width: '450px'}, 400);
     }
-    $('#city').keypress(function(){
+    $('#city, #search').keypress(function(){
         var search = $(this).val();
         if (search.length >= 2) {
-            $('.popupCitySelection__search').removeClass('search_hide').addClass('search_active');
+            $('.search').removeClass('search_hide').addClass('search_active');
+            $('#search').animate({width: '450px'}, 400);
         }
-        $('#city').focusout(function () {
-            $('.popupCitySelection__search').removeClass('search_active').addClass('search_hide');
+        $('#city, #search').focusout(function () {
+            $('.search').removeClass('search_active').addClass('search_hide');
+            $('#search').animate({width: '290px'}, 400);
         });
     });
 });
+
