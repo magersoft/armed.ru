a89d3d846e1dabb338805e6c7839a8d290d8acf3
yuvmironov <146@armed.ru>
2018-01-24 12:48:25 +0300

    Эффект при наведении

---
+++ app/include/Global/product/product.js
@@ -0,0 +1,14 @@
+$('.product').hover(
+    function() {
+        /*console.log('hover')*/
+        $(this).removeClass('product_nothover').addClass('product_hover');
+        $(this).children('.product__asside').removeClass('product__asside_hide').addClass('product__asside_active');
+        $(this).children('.product__buttonWrapp').removeClass('product__buttonWrapp_hide').addClass('product__buttonWrapp_active');
+    },
+    function(){
+        /*console.log('not-hover')*/
+        $(this).removeClass('product_hover').addClass('product_nothover');
+        $(this).children('.product__asside').removeClass('product__asside_active').addClass('product__asside_hide');
+        $(this).children('.product__buttonWrapp').removeClass('product__buttonWrapp_active').addClass('product__buttonWrapp_hide');
+    }
+);
