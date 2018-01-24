728c2b5346f475aabd12d3520acccb650be4db5d
Елизавета Савенко <129@armed.ru>
2018-01-24 13:04:41 +0300

    Модалки

--- app/include/popup/popupCheck/check/check.js
+++ app/include/popup/popupCheck/check/check.js
@@ -3,8 +3,10 @@ $(function () {
         var check = $('#checkbox').prop('checked');
         if (check) {
             $('#entity').prop('disabled', false);
+            $('.check__form-label--entity').css('color', '#828282');
         } else {
             $('#entity').prop('disabled', true);
+            $('.check__form-label--entity').css('color', '#bdbdbd')
         }
 
     });
