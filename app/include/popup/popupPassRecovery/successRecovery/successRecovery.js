eee1018b0ab3376685750521207a03f4820b6964
Елизавета Савенко <129@armed.ru>
2018-01-24 16:13:38 +0300

    комит

--- app/include/popup/popupPassRecovery/successRecovery/successRecovery.js
+++ app/include/popup/popupPassRecovery/successRecovery/successRecovery.js
@@ -3,5 +3,9 @@ $('#button__recovery').on('click', function(){
     console.log(content);
     $('.modal__change').removeClass('modal_view').addClass('modal_hide');
     $('.successRecovery').removeClass('successRecovery_hide').addClass('successRecovery_view');
+    $('#return').on('click', function(){
+        $('.modal__change').removeClass('modal_hide').addClass('modal_view');
+        $('.successRecovery').removeClass('successRecovery_view').addClass('successRecovery_hide');
+    });
     return false;
 });
