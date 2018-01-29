99df8e035bf1a958229a065c000a8ccdd2233f91
ARMED\mager_v <147@armed.ru>
2018-01-29 14:28:53 +0300

    Home page - 90%

---
+++ app/include/Global/countdown/countdown.js
@@ -0,0 +1,8 @@
+// До конца ферваля месяца
+$('#date_2802').countdown('2018/02/16').on('update.countdown', function(event) {
+    var $this = $(this).html(event.strftime(''
+        + '<span class="countdown-section"><span class="countdown-amount">%D</span> <span class="countdown-period">Дней</span> </span>'
+        + '<span class="countdown-section"><span class="countdown-amount">%H</span> <span class="countdown-period">Часов</span> </span>'
+        + '<span class="countdown-section"><span class="countdown-amount">%M</span> <span class="countdown-period">Минут</span> </span>'
+        + '<span class="countdown-section"><span class="countdown-amount">%S</span> <span class="countdown-period">Секунд</span> </span>'));
+});
