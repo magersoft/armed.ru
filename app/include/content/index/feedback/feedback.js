d003232ff728066a39995ae095a4976c199d2d88
Елизавета Савенко <129@armed.ru>
2018-01-29 18:00:51 +0300

    быстрый просмотр v2

---
+++ app/include/content/index/feedback/feedback.js
@@ -0,0 +1,10 @@
+$('.reviews__slider').slick({
+    nextArrow: '<i class="slide-right"></i>',
+    prevArrow: '<i class="slide-left"></i>',
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    variableWidth: false
+});
--- app/include/content/lk/edit/edit.js
+++ app/include/content/lk/edit/edit.js
@@ -6,4 +6,7 @@ $( function() {
         }
     });
 } );
-
+//TODO Создание темы для custom scrollbar
+$(".edit__users-body").mCustomScrollbar({
+    theme:"dark"
+});
