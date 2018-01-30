4b1d3fd6820a221a9585932535b8a9ce983642c8
Елизавета Савенко <129@armed.ru>
2018-01-30 13:53:34 +0300

    Merge branch 'master' of https://bitbucket.org/124armedru/armednewhtml
    # Conflicts:
    #	app/template/index.sass

--- app/include/popup/popupQuickView/quickView/quickView.js
+++ app/include/popup/popupQuickView/quickView/quickView.js
@@ -3,3 +3,11 @@
 $(".quickview-wrapper_description").mCustomScrollbar({
     theme:"dark"
 });
+
+
+$(".quickview__select").click(function () {
+    $(this).addClass('select__active');
+});
+$(".quickview__select").mouseout(function () {
+    $(this).removeClass('select__active');
+});
