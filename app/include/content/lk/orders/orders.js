bcf24ece8b8f5171663359d4050e011255a3514c
yuvmironov <146@armed.ru>
2018-01-31 12:21:24 +0300

    Личный кабинет. Верстка и стилизация фильтров на странице заказов в личном кабинете

--- app/include/content/lk/orders/orders.js
+++ app/include/content/lk/orders/orders.js
@@ -12,3 +12,14 @@ $( function() {
 $(".orders__body").mCustomScrollbar({
     theme:"dark"
 });
+
+$("#filterOpen").click(function () {
+    if($('#filterPopup').css('display') == 'none') {
+        $('#filterPopup').removeClass('orders__filter-popup_hide').addClass('orders__filter-popup_active');
+    } else {
+        $('#filterPopup').removeClass('orders__filter-popup_active').addClass('orders__filter-popup_hide');
+    }
+
+
+    return false
+});
