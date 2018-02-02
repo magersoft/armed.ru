baa347665616d8b2072dedb745a921073decbbc2
Елизавета Савенко <129@armed.ru>
2018-02-02 15:53:03 +0300

    Merge branch 'master' of https://bitbucket.org/124armedru/armednewhtml
    # Conflicts:
    #	app/include/content/pr_card/anchor/anchor.js

--- app/include/content/pr_card/set/set.js
+++ app/include/content/pr_card/set/set.js
@@ -1,11 +1,15 @@
 $('.set__slider').slick({
+    infinite: false,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 2,
     arrows: true,
     nextArrow: '<i class="slide-right slide-right-video"></i>',
     prevArrow: '<i class="slide-left slide-left-video"></i>'
 });
-$('.set__slider-check').click(function () {
-    $(this).toggleClass('set__slider-check-ok');
-    $('.set__slider-content').toggleClass('set-image_check')
+$('.set__slider-item__check').click(function () {
+    $(this).toggleClass('set__slider-item__check-ok');
+    var test = $(this).next().children();
+    $(test).toggleClass('set__check')
+});
+$('.set__slider-item__img').click(function () {
 });
