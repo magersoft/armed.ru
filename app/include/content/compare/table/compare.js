ab4c350252d9010afdc08a94e171b78a45281786
Елизавета Савенко <129@armed.ru>
2018-02-07 15:54:03 +0300

    Merge branch 'master' of https://bitbucket.org/124armedru/armednewhtml
    # Conflicts:
    #	app/include/content/pr_card/anchor/anchor.js

---
+++ app/include/content/compare/table/compare.js
@@ -0,0 +1,8 @@
+$(".scroll-row").mCustomScrollbar({
+    theme:"compare",
+    axis: "x",
+    advanced:{ autoExpandHorizontalScroll: true },
+    scrollbarPosition: "outside",
+    snapAmount: 207
+
+});
