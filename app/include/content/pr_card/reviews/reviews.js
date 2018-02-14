0413f412a7e3e6c90976a54c9b74e581e1ecc8ac
ARMED\mager_v <147@armed.ru>
2018-02-14 09:58:59 +0300

    Merge branch 'master' of https://bitbucket.org/124armedru/armednewhtml
    # Conflicts:
    #	app/include/content/lk/index/index.js

--- app/include/content/pr_card/reviews/reviews.js
+++ app/include/content/pr_card/reviews/reviews.js
@@ -1,12 +1,12 @@
 $('.percentage').easyPieChart({
     animate: 3000,
-    lineWidth: 10,
+    lineWidth: 7,
     onStart: $.noop,
-    onStop: $.noop
+    onStop: $.noop,
+    scaleColor: '#ffffff',
+    trackColor: '#e0e0e0',
+    barColor: '#f7c100'
 });
-
-
-
 var prec = $('.percentage').attr('data-percent');
 console.log(prec);
-$('.percentage').find('span').html(prec + ' %');
+$('.percentage').find('span').html(prec / '20');
--- app/include/content/pr_card/widget/widget.js
+++ app/include/content/pr_card/widget/widget.js
@@ -1,1 +1,18 @@
-$(".widget").stick_in_parent({offset_top: 140});
+var widget = $('.widget');
+console.log(widget);
+
+$(widget).stick_in_parent({offset_top: 140});
+
+/*$(window).scroll(function () {
+    var h = $('.widget');
+   if ( $(window).scrollTop() > 85){
+       h.height(720);
+       h.addClass('widget_animate');
+   }else {
+       h.height(600);
+   }
+});
+*/
+
+
+
