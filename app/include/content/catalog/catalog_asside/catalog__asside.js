d21da4418be2c3625de248c1e46f0e60694a1bf2
yuvmironov <146@armed.ru>
2018-02-16 09:56:40 +0300

    Чек бокс
    Правка скрипта

--- app/include/content/catalog/catalog_asside/catalog__asside.js
+++ app/include/content/catalog/catalog_asside/catalog__asside.js
@@ -5,7 +5,14 @@ $( ".FilterTabs" ).tabs({
     show: { effect: "blind", duration: 400 }
 });
 $('.FilterTabs-Label').change(function () {
-        var check = $('.FilterTabs-Label');
-        $(this).find("span").css('color', 'blue');
+        var color = $(this).find("span").css('color');
+
+        if (color == 'rgb(130, 130, 130)') {
+            $(this).find("span").css('color', 'blue');
+        } else if (color == 'rgb(0, 0, 255)') {
+            $(this).find("span").css('color', 'rgb(130, 130, 130)');
+        }
+
+
     });
 
