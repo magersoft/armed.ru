354c0f336881916a4987868e39b22c8c100afdfb
yuvmironov <146@armed.ru>
2018-02-19 16:22:11 +0300

    Фильтры заказов в личном кабинете
    - Оптимизирован скрипт для CustomSelect

--- app/include/Global/Select/Select.js
+++ app/include/Global/Select/Select.js
@@ -1,4 +1,31 @@
 //TODO Доработать скрипт см. в ежедневнике
+
+function CustomSelect (element, select_elements) {
+    console.log('Заходим в функцию');
+    console.log(element);
+    console.log(select_elements);
+    $(select_elements.children()).click(function () {
+        console.log('клик по ребенку');
+        console.log($(this));
+        $(element).html($(this).html());
+        $(element).next().css('display', 'none');
+        return false;
+    });
+
+}
+
+$(function () {
+    $('.Select-Head').on('click', function () {
+        $(this).next().css('display', 'flex');
+        var ob = $(this);
+        var select = $(this).next();
+        CustomSelect(ob, select);
+        return false;
+    });
+
+});
+
+/*
 $(function () {
     $('#SelectFilter').on('click', function(){
         $('.Select-Catalog').css('display', 'flex');
@@ -14,3 +41,19 @@ $(function () {
         return false;
     })
 });
+
+$(function () {
+    $('#SelectName').on('click', function(){
+        $('.Select-Catalog').css('display', 'flex');
+        console.log($('.Select-Catalog').css('display'));
+        $('.Select-Item').on('click', function(){
+
+            $('#SelectName').html($(this).html());
+            console.log($('#SelectName').html());
+            $('.Select-Catalog').css('display', 'none');
+            return false;
+
+        });
+        return false;
+    })
+});*/
