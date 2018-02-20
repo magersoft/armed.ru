f66a8fd646a82314ead9068f2d0ff481114bd269
Елизавета Савенко <129@armed.ru>
2018-02-20 09:37:05 +0300

    Правки по дизайну

--- app/include/content/pr_card/set/set.js
+++ app/include/content/pr_card/set/set.js
@@ -3,8 +3,8 @@ $('.set__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 2,
     arrows: true,
-    nextArrow: '<i class="slide-right slide-right-video"></i>',
-    prevArrow: '<i class="slide-left slide-left-video"></i>'
+    nextArrow: '<i class="slide-right slide-right-set"></i>',
+    prevArrow: '<i class="slide-left slide-left-set"></i>'
 });
 $('.set__slider-item__check').click(function () {
     $(this).toggleClass('set__slider-item__check-ok');
