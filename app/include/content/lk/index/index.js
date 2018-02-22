5140d1a801cee10c1fdc46b6d91b78ae8f1c94bb
mironov <146@armed.ru>
2018-02-22 10:45:51 +0300

    Личный кабинет
    - Оптимизация скрипта

--- app/include/content/lk/index/index.js
+++ app/include/content/lk/index/index.js
@@ -22,72 +22,61 @@ if (path == 'lk_index.html') {
     google.charts.setOnLoadCallback(drawMagazine);
     google.charts.setOnLoadCallback(drawAssortiment);
 
-    function drawMagazine() {
-        var data = google.visualization.arrayToDataTable([
-            ['Task', 'Hours per Day'],
+    //Опции для пирогов
+    var optionChart = {
+        legend :{
+            "position":"labeled",
+            "textStyle": {
+                "color": "black",
+                "fontSize": "14",
+                "format": "##%"
+            }
+        },
+        pieHole: 0.5,
+        colors: [
+            'rgb(151, 204, 100)',
+            'rgb(255, 217, 99)',
+            'rgb(253, 90, 62)',
+            'rgb(119, 182, 231)',
+            'rgb(169, 85, 184)',
+            'rgb(220, 157, 107)',
+            'rgb(234, 82, 127)',
+            'rgb(69, 105, 188)',
+            'rgb(75, 160, 98)'
+        ],
+        pieSliceText: 'none'
+    };
+
+    //Данные дял передачи в пироги
+    var magazineData =
+        [
+            ['Name', 'Count'],
             ['ООО "Дезиген"', 7],
             ['ИП "SEOGROUP"', 11],
             ['ООО "Верстальщик"', 5]
-        ]);
-        var options = {
-            legend :{
-                "position":"labeled",
-                "textStyle": {
-                    "color": "black",
-                    "fontSize": "14",
-                    "format": "##%"
-                }
-            },
-            pieHole: 0.5,
-            colors: [
-                'rgb(151, 204, 100)',
-                'rgb(255, 217, 99)',
-                'rgb(253, 90, 62)',
-                'rgb(119, 182, 231)',
-                'rgb(169, 85, 184)',
-                'rgb(220, 157, 107)',
-                'rgb(234, 82, 127)',
-                'rgb(69, 105, 188)',
-                'rgb(75, 160, 98)'
-            ],
-            pieSliceText: 'none'
-        };
-        var chart = new google.visualization.PieChart(document.getElementById('piechartMagazine'));
-        chart.draw(data, options);
-    }
-    function drawAssortiment() {
-        var data = google.visualization.arrayToDataTable([
-            ['Task', 'Hours per Day'],
+        ];
+    var assotimentData =
+        [
+            ['Name', 'Count'],
             ['Рециркуляторы', 11],
             ['Кислородные концентраторы', 6],
             ['Тонометры', 2],
             ['Увлажнители воздуха', 5],
             ['Другое', 20]
+        ];
 
-        ]);
-        var options = {
-            legend :{
-                "position":"labeled",
-                "textStyle": {
-                    "color": "black",
-                    "fontSize": "14",
-                    "format": "##%"
-                }
-            },
-            pieHole: 1,
-            colors: [
-                'rgb(151, 204, 100)',
-                'rgb(255, 217, 99)',
-                'rgb(253, 90, 62)',
-                'rgb(119, 182, 231)',
-                'rgb(169, 85, 184)',
-                'rgb(220, 157, 107)',
-                'rgb(234, 82, 127)',
-                'rgb(69, 105, 188)',
-                'rgb(75, 160, 98)'
-            ],
-            pieSliceText: 'none'
-        };
+    function drawMagazine() {
+
+        var data = google.visualization.arrayToDataTable(magazineData);
+        var options = optionChart;
+        var chart = new google.visualization.PieChart(document.getElementById('piechartMagazine'));
+        chart.draw(data, options);
+    }
+
+    function drawAssortiment() {
+
+        var data = google.visualization.arrayToDataTable(assotimentData);
+        var options = optionChart;
         var chart = new google.visualization.PieChart(document.getElementById('piechartAssortiment'));
         chart.draw(data, options)}
 
