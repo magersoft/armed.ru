eab15fd4a9357a8a05e14345e3e96e77cf2b37ee
mironov <146@armed.ru>
2018-02-26 19:08:12 +0300

    Сравнение товаров
    - Добавлен hover на строку таблицы

--- app/include/content/compare/table/compare.js
+++ app/include/content/compare/table/compare.js
@@ -13,3 +13,41 @@ $(".scroll-row").mCustomScrollbar({
     snapAmount: 207
 
 });
+
+$(function () {
+    $('.TrHover').hover(
+        function () {
+            //console.log("hover");
+            var data_id =$(this).attr('data-id');
+            //console.log(data_id);
+            var trElements = $(document).find('.TrHover');
+            //console.log(trElements);
+            //var itogElements = [];
+            for (var i = 0; i < trElements.length; i++){
+                //console.log(trElements[i]);
+                if ($(trElements[i]).attr('data-id') == data_id){
+                    $(trElements[i]).addClass('TrHover_Active');
+                    //console.log(trElements[i]);
+                    //itogElements += trElements[i];
+                }
+            }
+            //console.log(itogElements);
+        },
+        function () {
+            //console.log("not hover");
+            var data_id =$(this).attr('data-id');
+            //console.log(data_id);
+            var trElements = $(document).find('.TrHover');
+            //console.log(trElements);
+            //var itogElements = [];
+            for (var i = 0; i < trElements.length; i++){
+                //console.log(trElements[i]);
+                if ($(trElements[i]).attr('data-id') == data_id){
+                    $(trElements[i]).removeClass('TrHover_Active');
+                    //console.log(trElements[i]);
+                    //itogElements += trElements[i];
+                }
+            }
+        }
+    )
+});
