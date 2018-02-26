ae09ad77a21c80fe5208542f878094c70ac21a40
mironov <146@armed.ru>
2018-02-26 14:13:33 +0300

    Оформление заказа.
    - Замена выбора даты доставки на Custom

--- app/include/content/ordering/stepTwo/stepTwo_fiz.js
+++ app/include/content/ordering/stepTwo/stepTwo_fiz.js
@@ -11,3 +11,18 @@ $('#ReturnStepOne_Ur').click(function () {
 $('#StepThree_Ur').click(function () {
     document.location.href = "orderingStepThreeUr.html"
 });
+
+var DateOrderInput = document.getElementById('orderDate');
+
+if (DateOrderInput) {
+    var NowDate = new Date;
+    console.log(NowDate);
+    $('#orderDate').dateRangePicker({
+        language: 'ru',
+        singleDate: true,
+        autoClose: true,
+        singleMonth: true,
+        format: 'DD/MM/YYYY',
+        startDate: NowDate
+    });
+}
