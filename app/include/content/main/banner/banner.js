782bf634c6f06d34ff665330b18dc8363cfa5962
Елизавета Савенко <129@armed.ru>
2018-02-28 19:48:15 +0300

    промка главная

--- app/include/content/main/banner/banner.js
+++ app/include/content/main/banner/banner.js
@@ -1,5 +1,10 @@
-
 $('.banner__wrapper').click(function(){
 	$('.banner__wrapper').removeClass('banner-display');
 	$(this).toggleClass('banner-display');
+    $('.banner__item').removeClass('banner__item-display');
+	$(this).children().children().children().children('.banner__item').toggleClass('banner__item-display');
+    $('.banner__overlay').removeClass('banner__overlay-none');
+    $(this).children('.banner__overlay').toggleClass('banner__overlay-none');
+    // $('.banner-title').removeClass('banner-title-display');
+    // $(this).children().children().children('.banner-title').toggleClass('banner-title-display');
 });
