53f0f0fae6a530f7c931dd52e02120cb39322d21
mironov <146@armed.ru>
2018-03-01 12:10:56 +0300

    Оформление заказа
    - Валидация ИНН, БИК

--- app/include/content/ordering/stepTwo/stepTwo_fiz.js
+++ app/include/content/ordering/stepTwo/stepTwo_fiz.js
@@ -10,9 +10,7 @@ $('#ButtonStepThree').click(function () {
 $('#ReturnStepOne_Ur').click(function () {
     document.location.href = "orderingStepOneUr.html"
 });
-$('#StepThree_Ur').click(function () {
-    document.location.href = "orderingStepThreeUr.html"
-});
+
 
 var DateOrderInput = document.getElementById('orderDate');
 
