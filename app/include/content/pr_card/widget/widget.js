4b56e4b004807a90009ce5387909dcdb1f1ded7e
mironov <146@armed.ru>
2018-03-06 12:19:45 +0300

    Карточка товара
    - Решение для кнопок в виджете
    - Создвны два варианта кнопок для карточки товара

--- app/include/content/pr_card/widget/widget.js
+++ app/include/content/pr_card/widget/widget.js
@@ -1,8 +1,15 @@
 if(window.location.toString().indexOf('prcard_index.html')>0)
-{
-$(function() {
-	var widget = $('.widget');
-	$(widget).stick_in_parent({offset_top: 140});
-})
+	{
+		$(function() {
+			var widget = $('.widget');
+			$(widget).stick_in_parent({offset_top: 140});
+		});
+        var random = Math.round(Math.random());
+        console.log(random);
+		if (!random) {
+			$('.Tempory-BlockButtonChange_Basket').css('display', 'flex');
+		} else {
+            $('.Tempory-BlockButtonChange_Pred').css('display', 'flex');
+		}
+	}
 
-};
