b9450bb13decaaf80512c8935f12a3c5680e7a87
yuvmironov <146@armed.ru>
2018-03-07 10:23:50 +0300

    Карточка товара
    - Добвление блока с документами

---
+++ app/include/content/pr_card/Documents/Documents.js
@@ -0,0 +1,7 @@
+$('.Documents-Slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: '<i class="slide-right slide-right-video"></i>',
+    prevArrow: '<i class="slide-left slide-left-video"></i>'
+});
