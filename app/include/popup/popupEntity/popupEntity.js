42c7bf6569ebb386b0686c9f006cad7978f305b9
Елизавета Савенко <129@armed.ru>
2018-03-13 15:27:59 +0300

    Верстка модального окна для юридических лиц

---
+++ app/include/popup/popupEntity/popupEntity.js
@@ -0,0 +1,4 @@
+$('#ButtonModalEntity').on('click', function(){
+        $('.modal__change').removeClass('modal_view').addClass('modal_hide');
+        $('.successRecovery').removeClass('successRecovery_hide').addClass('successRecovery_view');
+});
