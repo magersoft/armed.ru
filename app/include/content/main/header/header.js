7d144e4e2615838b9e251457ef4a4ccc686cc914
Елизавета Савенко <129@armed.ru>
2018-03-15 15:32:34 +0300

    Дропдаун меню на промке

--- app/include/content/main/header/header.js
+++ app/include/content/main/header/header.js
@@ -14,3 +14,12 @@ $('.main-catalog__close--right').click(function(){
     $('.main-catalog--right').toggleClass('main-catalog--active');
     $('.main-catalog__wrapper--right').toggleClass('main-catalog__wrapper--active');
 });
+
+$('.main-catalog-top').click(function () {
+    var down = $(this).children('.main-catalog-bottom');
+    $('.main-catalog-bottom').not(down).slideUp(200);
+    down.slideToggle(200);
+    e.preventDefault();
+    // $('.main-catalog-bottom').slideUp(200);
+    console.log(down);
+});
