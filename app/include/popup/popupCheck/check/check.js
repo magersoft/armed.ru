7fa8d4f4233dc42ad934ae3ce19d853365037512
yuvmironov <146@armed.ru>
2018-03-15 15:14:08 +0300

    Окно регистрации
    - Исправлен косяк с активацией поля для юр лица

--- app/include/popup/popupCheck/check/check.js
+++ app/include/popup/popupCheck/check/check.js
@@ -2,10 +2,12 @@ $(function () {
     $('#checkbox').click(function () {
         var check = $('#checkbox').prop('checked');
         if (check) {
-            $('#entity', '.control-checkbox').prop('disabled', false);
+            $('#entity, .control-checkbox').prop('disabled', false);
+            console.log("false");
             $('.check__form-label--entity').css('color', '#828282');
         } else {
-            $('#entity', '.control-checkbox').prop('disabled', true);
+            $('#entity, .control-checkbox').prop('disabled', true);
+            console.log("true");
             $('.check__form-label--entity').css('color', '#bdbdbd')
         }
     });
