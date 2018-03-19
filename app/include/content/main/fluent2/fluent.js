2466f849075a2e00553d97d1e05f08e22faea846
Елизавета Савенко <129@armed.ru>
2018-03-19 16:34:22 +0300

    фиксация кнопки на банерев мейне

--- app/include/content/main/fluent2/fluent.js
+++ app/include/content/main/fluent2/fluent.js
@@ -2,10 +2,11 @@ var change = $('.fluent__info-business');
 var background = $('.background-business');
 $('.fluent__item-card').click(function () {
     var id = $(this).attr('id');
-    var fon = ('.fluent__inner-background');
+
+
     if (id == "fluentBusiness"){
         $(change).removeClass('bg-on').addClass('bg-off');
-        $(background).stop().animate({'opacity': '0'},500);
+        $(background).stop().animate({'opacity': '0'},2000);
 
         change = $('.fluent__info-business');
         function visibleblock() {
@@ -13,13 +14,15 @@ $('.fluent__item-card').click(function () {
         }
         setTimeout(visibleblock, 840);
         background = $('.background-business');
-        $(background).stop().animate({'opacity': '1'},500);
+        $(background).stop().animate({'opacity': '1'},2000);
         $('.fluent__item-card').removeClass('fluent__active');
-        $(this).toggleClass('fluent__active');
+
+
+        $('.fluent__button').removeClass('fluent__button-home').removeClass('fluent__button-care').removeClass('fluent__button-pro').addClass('fluent__button-business').attr('href','promo_bus.html');
     }
     if (id == "fluentHome"){
         $(change).removeClass('bg-on').addClass('bg-off');
-        $(background).stop().animate({'opacity': '0'},500);
+        $(background).stop().animate({'opacity': '0'},2000);
         change = $('.fluent__info-home');
 
         function visibleblock() {
@@ -27,36 +30,44 @@ $('.fluent__item-card').click(function () {
         }
         setTimeout(visibleblock, 840);
         background = $('.background-home');
-        $(background).stop().animate({'opacity': '1'},500);
-        $('.fluent__item-card').removeClass('fluent__active');
-        $(this).toggleClass('fluent__active');
+        $(background).stop().animate({'opacity': '1'},2000);
+
+        $('.fluent__button').removeClass('fluent__button-business').removeClass('fluent__button-care').removeClass('fluent__button-pro').addClass('fluent__button-home').attr('href','error.html');
     }
     if (id == "fluentCare"){
         $(change).removeClass('bg-on').addClass('bg-off');
         $(change).removeClass('bg-on').addClass('bg-off');
-        $(background).stop().animate({'opacity': '0'},500);
+
+        $(background).stop().animate({'opacity': '0'},2000);
+
         change = $('.fluent__info-care');
         function visibleblock() {
             $(change).removeClass('bg-off').addClass('bg-on');
         }
         setTimeout(visibleblock, 840);
         $(change).removeClass('bg-on').addClass('bg-off');
+
+
         background = $('.background-care');
-        $(background).stop().animate({'opacity': '1'},500);
-        $('.fluent__item-card').removeClass('fluent__active');
-        $(this).toggleClass('fluent__active');
+        $(background).stop().animate({'opacity': '1'},2000);
+
+
+        $('.fluent__button').removeClass('fluent__button-home').removeClass('fluent__button-business').removeClass('fluent__button-pro').addClass('fluent__button-care').attr('href','error.html');
     }
-    if (id == "fluentPro"){
+    if (id == "fluentPro") {
         $(change).removeClass('bg-on').addClass('bg-off');
-        $(background).stop().animate({'opacity': '0'},500);
+        $(background).stop().animate({'opacity': '0'}, 2000);
         change = $('.fluent__info-pro');
+
         function visibleblock() {
             $(change).removeClass('bg-off').addClass('bg-on');
         }
+
         setTimeout(visibleblock, 840);
         background = $('.background-pro');
-        $(background).stop().animate({'opacity': '1'},500);
-     }
+        $(background).stop().animate({'opacity': '1'}, 2000);
+        $('.fluent__button').removeClass('fluent__button-home').removeClass('fluent__button-care').removeClass('fluent__button-business').addClass('fluent__button-pro').attr('href','error.html');
+    }
     $('.fluent__item-card').removeClass('fluent__active');
     $(this).toggleClass('fluent__active');
 });
