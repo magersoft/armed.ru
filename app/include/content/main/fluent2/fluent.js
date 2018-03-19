940ea50de9f33274c64be119f4d2c41fc1f4fae9
Елизавета Савенко <129@armed.ru>
2018-03-19 11:46:28 +0300

    Плавное появление и исчезание фона, подмена выезжающие и приезжающие блоки

--- app/include/content/main/fluent2/fluent.js
+++ app/include/content/main/fluent2/fluent.js
@@ -1,88 +1,61 @@
 var change = $('.fluent__info-business');
+var background = $('.background-business');
 $('.fluent__item-card').click(function () {
     var id = $(this).attr('id');
     var fon = ('.fluent__inner-background');
     if (id == "fluentBusiness"){
-        $(change).animate({"right": "+=700px"}, "slow");
-        function animationStart() {
-            $(fon).stop().animate({'opacity': '0'},500);
-        }
-        setTimeout(animationStart,10);
-        function removeClass() {
-            $(fon).stop().css('background-image','url(images/main/fluent/bis-big-bg.png)');
-        }
-        setTimeout(removeClass, 600);
-        function animationStop() {
-            $(fon).stop().animate({'opacity':'1'},300);
-        }
+        $(change).removeClass('bg-on').addClass('bg-off');
+        $(background).animate({'opacity': '0'},500);
+
         change = $('.fluent__info-business');
-        $(change).animate({"right": "-=700px"}, "slow");
-        setTimeout(animationStop, 700);
-        // $('.fluent__info').removeClass('fluent__info-hide');
-        // $('.fluent__info-business').addClass('fluent__info-hide');
+        function visibleblock() {
+            $(change).removeClass('bg-off').addClass('bg-on');
+        }
+        setTimeout(visibleblock, 840);
+        background = $('.background-business');
+        $(background).animate({'opacity': '1'},500);
+        $('.fluent__item-card').removeClass('fluent__active');
+        $(this).toggleClass('fluent__active');
     }
     if (id == "fluentHome"){
-        $(change).animate({"right": "+=700px"}, "slow");
-        function animationStart() {
-            $(fon).stop().animate({'opacity': '0'},500);
-        }
-        function removeClass() {
-            $(fon).stop().css('background-image','url(images/main/fluent/home-big-bg.png)');
-        }
-        function animationStop() {
-            $(fon).stop().animate({'opacity':'1'},300);
-        }
+        $(change).removeClass('bg-on').addClass('bg-off');
+        $(background).animate({'opacity': '0'},500);
         change = $('.fluent__info-home');
-        $(change).animate({"right": "-=700px"}, "slow");
-        setTimeout(animationStart,10);
-        setTimeout(removeClass, 600);
-        setTimeout(animationStop, 700);
-        // $('.fluent__info').removeClass('fluent__info-hide');
-        // $('.fluent__info-home').addClass('fluent__info-hide');
+
+        function visibleblock() {
+            $(change).removeClass('bg-off').addClass('bg-on');
+        }
+        setTimeout(visibleblock, 840);
+        background = $('.background-home');
+        $(background).animate({'opacity': '1'},500);
+        $('.fluent__item-card').removeClass('fluent__active');
+        $(this).toggleClass('fluent__active');
     }
     if (id == "fluentCare"){
-        $(change).css('margin-left', '-700px');
-        function animationStart() {
-            $(fon).stop().animate({'opacity': '0'},500);
-        }
-        function removeClass() {
-            $(fon).css('background-image','url(\'images/main/fluent/care-big-bg.png\'');
-        }
-        function animationStop() {
-            $(fon).stop().animate({'opacity':'1'},300);
-        }
+        $(change).removeClass('bg-on').addClass('bg-off');
+        $(change).removeClass('bg-on').addClass('bg-off');
+        $(background).animate({'opacity': '0'},500);
         change = $('.fluent__info-care');
-        $(change).css('margin-left', '0');
-        setTimeout(animationStart,10);
-        setTimeout(removeClass, 600);
-        setTimeout(animationStop, 700);
-        // $('.fluent__info').removeClass('fluent__info-hide');
-        // $('.fluent__info-care').addClass('fluent__info-hide');
+        function visibleblock() {
+            $(change).removeClass('bg-off').addClass('bg-on');
+        }
+        setTimeout(visibleblock, 840);
+        $(change).removeClass('bg-on').addClass('bg-off');
+        background = $('.background-care');
+        $(background).animate({'opacity': '1'},500);
+        $('.fluent__item-card').removeClass('fluent__active');
+        $(this).toggleClass('fluent__active');
     }
     if (id == "fluentPro"){
-        $(change).css('margin-left', '-700px');
-        function animationStart() {
-            $(fon).stop().animate({'opacity': '0'},500);
+        $(change).removeClass('bg-on').addClass('bg-off');
+        $(background).animate({'opacity': '0'},500);
+        function visibleblock() {
+            $(change).removeClass('bg-off').addClass('bg-on');
         }
-        function removeClass() {
-            $(fon).css('background-image','url(\'images/main/fluent/pro-big-bg.png\'');
-        }
-        function animationStop() {
-            $(fon).stop().animate({'opacity':'1'},300);
-        }
-        change = $('.fluent__info-pro');
-        $(change).css('margin-left', '0');
-        setTimeout(animationStart,10);
-        setTimeout(removeClass, 600);
-        setTimeout(animationStop, 700);
-        // $('.fluent__info').removeClass('fluent__info-hide');
-        // $('.fluent__info-pro').addClass('fluent__info-hide');
+        setTimeout(visibleblock, 840);
+        background = $('.background-pro');
+        $(background).animate({'opacity': '1'},500);
      }
     $('.fluent__item-card').removeClass('fluent__active');
     $(this).toggleClass('fluent__active');
-    // $('.fluent__inner-background').css('background', fon);
-    // $('.fluent__inner-background').animate({'opacity': '0.7'}, 400);
-    // $('.fluent__inner-background').animate({'opacity': '1'}, 400);
-    //$(this)
-    //console.log(fon);
 });
