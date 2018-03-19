156c9dfe4db63b18621b2160df3b06916c713a88
yuvmironov <146@armed.ru>
2018-03-19 09:58:45 +0300

    Катрочка товара
    - Дублирование скрипта (убрано)

--- app/include/content/pr_card_ur/same/same.js
+++ app/include/content/pr_card_ur/same/same.js
@@ -1,10 +1,1 @@
-$('.same__slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    variableWidth: true,
-    arrows: true,
-    infinite: false,
-    adaptiveHeight: true,
-    nextArrow: '<i class="slide-right slide-right-same"></i>',
-    prevArrow: '<i class="slide-left slide-left-same"></i>'
-});
+//Используется js из pr_card
--- app/include/content/pr_card_ur/video/video.js
+++ app/include/content/pr_card_ur/video/video.js
@@ -1,7 +1,1 @@
-$('.video__slider').slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    arrows: true,
-    nextArrow: '<i class="slide-right slide-right-video"></i>',
-    prevArrow: '<i class="slide-left slide-left-video"></i>'
-});
+//Используется js из pr_card
