4f41ea2e3c7d1a1441fb96e7a2b4c35f8a096dd3
Елизавета Савенко <129@armed.ru>
2018-03-19 12:47:04 +0300

    баннер-текст

--- app/include/content/main/fluent2/fluent.js
+++ app/include/content/main/fluent2/fluent.js
@@ -5,7 +5,7 @@ $('.fluent__item-card').click(function () {
     var fon = ('.fluent__inner-background');
     if (id == "fluentBusiness"){
         $(change).removeClass('bg-on').addClass('bg-off');
-        $(background).animate({'opacity': '0'},500);
+        $(background).stop().animate({'opacity': '0'},500);
 
         change = $('.fluent__info-business');
         function visibleblock() {
@@ -13,13 +13,13 @@ $('.fluent__item-card').click(function () {
         }
         setTimeout(visibleblock, 840);
         background = $('.background-business');
-        $(background).animate({'opacity': '1'},500);
+        $(background).stop().animate({'opacity': '1'},500);
         $('.fluent__item-card').removeClass('fluent__active');
         $(this).toggleClass('fluent__active');
     }
     if (id == "fluentHome"){
         $(change).removeClass('bg-on').addClass('bg-off');
-        $(background).animate({'opacity': '0'},500);
+        $(background).stop().animate({'opacity': '0'},500);
         change = $('.fluent__info-home');
 
         function visibleblock() {
@@ -27,14 +27,14 @@ $('.fluent__item-card').click(function () {
         }
         setTimeout(visibleblock, 840);
         background = $('.background-home');
-        $(background).animate({'opacity': '1'},500);
+        $(background).stop().animate({'opacity': '1'},500);
         $('.fluent__item-card').removeClass('fluent__active');
         $(this).toggleClass('fluent__active');
     }
     if (id == "fluentCare"){
         $(change).removeClass('bg-on').addClass('bg-off');
         $(change).removeClass('bg-on').addClass('bg-off');
-        $(background).animate({'opacity': '0'},500);
+        $(background).stop().animate({'opacity': '0'},500);
         change = $('.fluent__info-care');
         function visibleblock() {
             $(change).removeClass('bg-off').addClass('bg-on');
@@ -42,19 +42,20 @@ $('.fluent__item-card').click(function () {
         setTimeout(visibleblock, 840);
         $(change).removeClass('bg-on').addClass('bg-off');
         background = $('.background-care');
-        $(background).animate({'opacity': '1'},500);
+        $(background).stop().animate({'opacity': '1'},500);
         $('.fluent__item-card').removeClass('fluent__active');
         $(this).toggleClass('fluent__active');
     }
     if (id == "fluentPro"){
         $(change).removeClass('bg-on').addClass('bg-off');
-        $(background).animate({'opacity': '0'},500);
+        $(background).stop().animate({'opacity': '0'},500);
+        change = $('.fluent__info-pro');
         function visibleblock() {
             $(change).removeClass('bg-off').addClass('bg-on');
         }
         setTimeout(visibleblock, 840);
         background = $('.background-pro');
-        $(background).animate({'opacity': '1'},500);
+        $(background).stop().animate({'opacity': '1'},500);
      }
     $('.fluent__item-card').removeClass('fluent__active');
     $(this).toggleClass('fluent__active');
