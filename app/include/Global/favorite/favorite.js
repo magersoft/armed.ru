7d90eac058a22089bbd0402a36dd8a98f9bdb4f9
Елизавета Савенко <129@armed.ru>
2018-03-21 15:40:57 +0300

    прожатие избранного

---
+++ app/include/Global/favorite/favorite.js
@@ -0,0 +1,8 @@
+$('.table-icon__like, .product__icon_favorite').click(function () {
+    //e.preventDefault();
+    $(this).toggleClass('likeAdd');
+});
+$('.quickview-inner_buy__icons-like').click(function () {
+    //e.preventDefault();
+    $(this).toggleClass('likeAdd-Big');
+});
