26c0b134c96fd269a4edf9d4e6e6cb92d895e18d
Елизавета Савенко <129@armed.ru>
2018-03-22 11:11:09 +0300

    favorite прожатие

--- app/include/Global/favorite/favorite.js
+++ app/include/Global/favorite/favorite.js
@@ -6,3 +6,7 @@ $('.quickview-inner_buy__icons-like').click(function () {
     //e.preventDefault();
     $(this).toggleClass('likeAdd-Big');
 });
+$(' .product__icon_favorite_list').click(function () {
+    //e.preventDefault();
+    $(this).toggleClass('likeNone');
+});
