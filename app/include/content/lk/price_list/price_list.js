c1954b39dd4cbb13a58d12d9a5dc82744796c2eb
yuvmironov <146@armed.ru>
2018-03-23 16:08:10 +0300

    Личный кабинет - Общий прайс
    - Создана страница общего прайса с возможностью купить

---
+++ app/include/content/lk/price_list/price_list.js
@@ -0,0 +1,8 @@
+$(".PriceList-Content").mCustomScrollbar({
+    theme:"lkprice",
+    scrollbarPosition: "outside"
+});
+$(".PriceList-InfoMain").mCustomScrollbar({
+    theme:"lkprice",
+    scrollbarPosition: "outside"
+});
